a30a0754abaceb4b8eca41c8f6cec8b626cd870e
Socrat Mohammad <127431597+socratmhamad@users.noreply.github.com>
2026-07-24 18:24:36 +0300

    Fix formatting of allowed origins in app.py

--- backend/app.py
+++ backend/app.py
@@ -79,7 +79,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://telegram-movie-library.vercel.app"
+        "https://telegram-movie-library.vercel.app",
         "https://socfix.tech",
     ],
     allow_origin_regex=r"https?://(localhost|127\.0\.0\.1|192\.168\.\d+\.\d+|10\.\d+\.\d+\.\d+)(:\d+)?",
